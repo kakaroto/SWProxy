58ad89767727854141f68e2026a4c5401439e81a
dozens <dozens@pobox.com>
2016-01-07 17:39:24 -0500

    fixed bug in rune set determination

--- SWParser/dozens.py
+++ SWParser/dozens.py
@@ -80,8 +80,8 @@ def rune_addition(runes, stat, basevalue):
 def get_rune_sets(runes):
     from parser import rune_set_id
     
-#                    1  2  3  4  5  6  7  8  9  A  B  C  D  E  F 11 12
-    rune_sets_min = [2, 2, 4, 2, 4, 2, 2, 4, 6, 4, 4, 6, 2, 2, 2, 2, 2]
+#                    1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18
+    rune_sets_min = [2, 2, 4, 2, 4, 2, 2, 4, 6, 4, 4, 6, 4, 2, 2, 2, 2, 2]
     rune_sets = [0] * 18
 
     if len(runes) == 0:
