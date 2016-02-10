1bc5873c8bc72e4d0f2b87d55b61a1c8102dcf5a
Youness Alaoui <kakaroto@kakaroto.homelinux.net>
2016-02-09 20:12:31 -0500

    Show more info for Unit Collection plugin

--- plugins/PrintUnitCollection.py
+++ plugins/PrintUnitCollection.py
@@ -1,13 +1,20 @@
 import SWPlugin
+from SWParser.parser import monster_attribute
 
+logger = logging.getLogger("SWProxy")
 
 class PrintUnitCollection(SWPlugin.SWPlugin):
 
     def process_request(self, req_json, resp_json):
-
         if resp_json.get('command') == 'GetUnitCollection':
             collection = resp_json['collection']
-            print "Your collection has {}/{} monsters".format(
+            logger.info("Your collection has {}/{} monsters".format(
                 sum([y['open'] for y in collection]),
                 len(collection),
-            )
+            ))
+            for i in xrange(1, 6):
+                logger.info("You found {}/{} {} monsters".format(
+                    sum([y['open'] if str(y['unit_master_id'])[-1] == str(i) else 0 for y in collection]),
+                    sum([1 if str(y['unit_master_id'])[-1] == str(i) else 0 for y in collection]),
+                    monster_attribute(i)
+                ))
