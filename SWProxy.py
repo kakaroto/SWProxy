e5e1f5d3da32cda06ea28cbc6008333e1c37c526
Leonardo Stern <leonardo.stern@gmail.com>
2016-03-29 13:26:33 -0300

    trying to fix local ip detection

--- SWProxy.py
+++ SWProxy.py
@@ -111,22 +111,13 @@ def get_external_ip():
 
     # ref: http://stackoverflow.com/a/1267524
     # excluding reserved ip ranges defined on http://tools.ietf.org/html/rfc5735
-    try:
-        sockets = [[(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]
-        ordered_list = sorted(socket.gethostbyname_ex(socket.gethostname())[2], key=priority)
-        ips = [l for l in ([ip for ip in ordered_list if
-                            not address_in_network(ip, "127.0.0.0", 8) and not address_in_network(ip, "169.254.0.0", 16) and
-                            not address_in_network(ip, "172.16.0.0", 12) and not address_in_network(ip, "192.0.0.0", 12) and
-                            not address_in_network(ip, "192.0.2.0", 24) and not address_in_network(ip, "192.88.99.0", 24) and
-                            not address_in_network(ip, "198.18.0.0", 15)][:1], sockets) if l]
-        return ips[0][0]
-    except (KeyError, socket.gaierror):
-        try:
-            # first fallback
-            return socket.gethostbyname(socket.gethostname())
-        except (KeyError, socket.gaierror):
-            # sometimes, this just works, fixed OSX
-            return sockets[0]
+    sockets = [[(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]
+    ordered_list = sorted(sockets, key=priority)
+    ips = [ip for ip in ordered_list if
+                        not address_in_network(ip, "127.0.0.0", 8) and not address_in_network(ip, "169.254.0.0", 16) and
+                        not address_in_network(ip, "172.16.0.0", 12) and not address_in_network(ip, "192.0.2.0", 24) and
+                        not address_in_network(ip, "192.88.99.0", 24) and not address_in_network(ip, "198.18.0.0", 15)]
+    return ips[0]
 
 
 def read_file_lines(fpath):
